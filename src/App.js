51cb1bb9e6f3115378ef2cd0c0a2cd7d343490b8
Eckersid Silapaswang <esilapswang@advancelocal.net>
2020-09-15 21:38:40 -0400

    Init cut at auth

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import CharacterSheet from './characterSheet';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 
 function App() {
   return (
     <div className="App">
+        <AmplifySignOut />
         <CharacterSheet/>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
