60dce64e6c506b096d26fd0d292140ac7224cec2
Eckersid Silapaswang <esilapswang@advancelocal.net>
2020-10-23 11:35:39 -0400

    Init cut at ui overhaul

--- src/App.js
+++ src/App.js
@@ -1,69 +1,68 @@
 import React, { useState, useEffect } from 'react';
-import { NavLink, Redirect, Route, Switch, useHistory } from 'react-router-dom';
-import { Auth } from 'aws-amplify';
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
-import CharacterSheet from './characterSheet';
-import Visuals from "./Visuals";
+import { Redirect, Route, Switch } from 'react-router-dom';
+import { API, Auth } from 'aws-amplify';
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import { listCharacterSheets } from './graphql/queries';
+import MainContent from './components/MainContent';
+import Sidebar from './components/Sidebar';
 import './App.css';
-import {AppBar, Tab, Tabs, Toolbar} from "@material-ui/core";
 
 const UserContext = React.createContext("");
 
 function InitApp() {
-    let history = useHistory();
-    const [user, setUser] = useState("");
-    const [activeTab, setActiveTab] = useState("charactersheet");
+    const contentSourceNames = ["character", "skills", "weapons", "talents", "visuals"];
+    const [userCharacterSheets, setUserCharacterSheets] = useState({user: null, characterSheets: []});
+    const [activeIndex, setActiveIndex] = useState(0);
 
     useEffect(() => {
-        Auth.currentAuthenticatedUser({
-            bypassCache: false
-        }).then(user => {
-            console.log(`USER: ${user.username}`);
-            setUser(user.username);
-        });
+        fetchUserCharacterSheets();
     }, []);
 
-    function handleTabChange(e, v) {
-        e.preventDefault();
-        setActiveTab(v);
-        history.push(`/${v}`);
+    async function fetchUserCharacterSheets() {
+        const { username } = await Auth.currentAuthenticatedUser({bypassCache: false});
+        const apiData = username && await API.graphql({
+            query: listCharacterSheets,
+            variables: {
+                filter: {
+                    user: {
+                        eq: username
+                    }
+                }
+            }
+        });
+        setUserCharacterSheets({user: username, characterSheets: apiData.data.listCharacterSheets.items});
     }
 
-    return user ? (
-        <UserContext.Provider value={user}>
+    return (
+        <UserContext.Provider value={userCharacterSheets.user}>
             <div className="App">
-                <div className="top__nav">
-                    <AppBar position="static">
-                        <Toolbar>
-                            <Tabs
-                                value={activeTab}
-                                onChange={handleTabChange}
-                                aria-label="simple tabs example"
-                            >
-                                <Tab value={"charactersheet"} label="Character Sheet"/>
-                                <Tab value={"visuals"} label="Visuals"/>
-                            </Tabs>
-                            <div style={{flex: 1}}></div>
-                            <AmplifySignOut />
-                        </Toolbar>
-                    </AppBar>
+                <div className="app__body">
+                    <Sidebar
+                        contentSourceNames={contentSourceNames}
+                        userCharacterSheets={userCharacterSheets.characterSheets}
+                        activeIndex={activeIndex}
+                        setActiveIndex={setActiveIndex}
+                    />
+                    <Switch>
+                        {contentSourceNames.map((name, idx) => {
+                            return (
+                                <Route key={idx} path={`/${name}`}>
+                                    <MainContent
+                                        userCharacterSheets={userCharacterSheets}
+                                        activeIndex={activeIndex}
+                                        setActiveIndex={setActiveIndex}
+                                    />
+                                </Route>
+                            )
+                        })}
+                        <Route>
+                            <Redirect to="/visuals" />
+                        </Route>
+                    </Switch>
                 </div>
-                <Switch>
-                    <Route path={`/charactersheet`}>
-                        <CharacterSheet/>
-                    </Route>
-                    <Route path={`/visuals`}>
-                        <Visuals/>
-                    </Route>
-                    <Route>
-                        <Redirect to="/charactersheet"/>
-                    </Route>
-                </Switch>
             </div>
         </UserContext.Provider>
     )
-        :
-        <div></div>
 
 }
 
